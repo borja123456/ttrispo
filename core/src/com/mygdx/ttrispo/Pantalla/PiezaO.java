2ca43ca3efb2a815c10dc9b1c7f69a8daad49891
Muffinous <mimiafsx@outlook.es>
2019-10-03 14:24:52 +0200

    Pieza I y O, tipos de pieza en 'Pieza'.

--- core/src/com/mygdx/ttrispo/Pantalla/PiezaO.java
+++ core/src/com/mygdx/ttrispo/Pantalla/PiezaO.java
@@ -1,80 +1,73 @@
 package com.mygdx.ttrispo.Pantalla;
 
-public class Pieza {
+public class PiezaO {
 
-    public static final int T = 1;
-    public static final int S = 2;
-    public static final int Z = 3;
-    public static final int J = 4;
-    public static final int L = 5;
-    public static final int I = 6;
     public static final int O = 7;
 
     public static final int VACIA = 0;
 
-    protected int f,c, numBlock;
+    protected int f, c, numBlock;
     private int giro = 1;
     private int tipo;
 
-    public Pieza(int f,int c) {
+    public PiezaO(int f, int c) {
         this.f = f;
         this.c = c;
-        tipo = Pieza.T;
+        tipo = Pieza.O;
         this.numBlock = 4;
     }
 
-    protected int [][] getPosicionPieza(){
-        int [][] r = new int[numBlock][2];
-        switch (giro){
-            case(1):
-                //Bloque 1
+    protected int[][] getPosicionPieza() {
+        int[][] r = new int[numBlock][2];
+        switch (giro) {
+            case (1): // 0º -> 90º  (orientacion 1 a orientacion 2)
                 r[0][0] = this.f; // Fila
                 r[0][1] = this.c - 1; // Columna
 
                 r[1][0] = this.f;
                 r[1][1] = this.c;
 
-                r[2][0] = this.f;
-                r[2][1] = this.c + 1;
+                r[2][0] = this.f + 1;
+                r[2][1] = this.c - 1;
 
                 r[3][0] = this.f + 1;
                 r[3][1] = this.c;
                 break;
-            case(2):
+            case (2): // 90º -> 180º  (orientacion 2 a orientacion 3)
                 r[0][0] = this.f; // Fila
                 r[0][1] = this.c; // Columna
 
-                r[1][0] = this.f - 1;
+                r[1][0] = this.f + 1;
                 r[1][1] = this.c;
 
                 r[2][0] = this.f + 1;
-                r[2][1] = this.c;
+                r[2][1] = this.c - 1;
 
                 r[3][0] = this.f;
                 r[3][1] = this.c - 1;
                 break;
-            case(3):
-                r[0][0] = this.f; // Fila
+            case (3): // 180º -> 270º  (orientacion 3 a orientacion 4)
+                r[0][0] = this.f + 1; // Fila
                 r[0][1] = this.c; // Columna
+
+                r[1][0] = this.f + 1;
+                r[1][1] = this.c - 1;
+
+                r[2][0] = this.f;
+                r[2][1] = this.c - 1;
+
+                r[3][0] = this.f;
+                r[3][1] = this.c;
+                break;
+            case (4): // 270º -> 0º  (orientacion 4 a orientacion 1)
+                r[0][0] = this.f + 1; // Fila
+                r[0][1] = this.c - 1; // Columna
 
                 r[1][0] = this.f;
                 r[1][1] = this.c - 1;
 
-                r[2][0] = this.f - 1;
-                r[2][1] = this.c;
-
-                r[3][0] = this.f;
-                r[3][1] = this.c + 1;
-                break;
-            case(4):
-                r[0][0] = this.f; // Fila
-                r[0][1] = this.c; // Columna
-
-                r[1][0] = this.f - 1;
-                r[1][1] = this.c;
-
                 r[2][0] = this.f;
-                r[2][1] = this.c + 1;
+                r[2][1] = this.c;
 
                 r[3][0] = this.f + 1;
                 r[3][1] = this.c;
@@ -83,8 +76,8 @@ public class Pieza {
         return r;
     }
 
-    public int [][] getPosicionAbajo(){
-        int [][] r = this.getPosicionPieza();
+    public int[][] getPosicionAbajo() {
+        int[][] r = this.getPosicionPieza();
         for (int i = 0; i < r.length; i++) {
             r[i][0]++;
         }
