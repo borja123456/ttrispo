1b7f2f01dd8a308b0a79222a27dccf1be65c6963
lordkener <55380975+lordkener@users.noreply.github.com>
2019-10-12 20:58:37 +0200

    puntuacion on

--- core/src/com/mygdx/ttrispo/Pantalla/ProgresoPartida.java
+++ core/src/com/mygdx/ttrispo/Pantalla/ProgresoPartida.java
@@ -4,24 +4,21 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.g2d.Batch;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.scenes.scene2d.Actor;
-import com.mygdx.ttrispo.Tablero;
 
 public class ProgresoPartida extends Actor {
     private BitmapFont font;
     private Partida partida;
-    private Tablero tablero;
 
-    public ProgresoPartida(Partida partida, Tablero tablero){
+    public ProgresoPartida(Partida partida){
         this.partida = partida;
-        this.font = new BitmapFont();
-        this.tablero = tablero;
+        font = new BitmapFont();
     }
 
     @Override
     public void draw(Batch batch, float parentAlpha) {
         super.draw(batch, parentAlpha);
         font.getData().setScale(8);
-        font.draw(batch, ""+tablero.getPuntuacion(),
-                Gdx.graphics.getWidth()/5, 94*Gdx.graphics.getHeight()/3);
+        font.draw(batch, ""+partida.getPuntuacion(),
+                Gdx.graphics.getWidth()/5, 93*Gdx.graphics.getHeight()/100);
     }
 }
