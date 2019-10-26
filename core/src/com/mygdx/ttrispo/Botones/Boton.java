606cd710d0a34da03c2833ee58da2a3cc2ec241b
JorgeCarreras2 <jorgecarrerasmartin@hotmail.com>
2019-10-26 12:57:43 +0200

    Boton

---
+++ core/src/com/mygdx/ttrispo/Botones/Boton.java
@@ -0,0 +1,12 @@
+package com.mygdx.ttrispo.Botones;
+
+import com.badlogic.gdx.scenes.scene2d.ui.Skin;
+import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
+
+public class Boton extends TextButton {
+
+
+    public Boton(String text, Skin skin) {
+        super(text, skin);
+    }
+}
