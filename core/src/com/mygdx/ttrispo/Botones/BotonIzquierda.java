0938940dbae369a1c2a51e42508f3d02c52850fe
JorgeCarreras2 <jorgecarrerasmartin@hotmail.com>
2019-10-27 18:47:16 +0100

    dqd

--- core/src/com/mygdx/ttrispo/Botones/BotonIzquierda.java
+++ core/src/com/mygdx/ttrispo/Botones/BotonIzquierda.java
@@ -2,12 +2,18 @@ package com.mygdx.ttrispo.Botones;
 
 import com.badlogic.gdx.scenes.scene2d.ui.Skin;
 import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
+import com.mygdx.ttrispo.MyGdxGame;
 
 public class BotonIzquierda extends TextButton {
-    private static int POSICION_X=45;
-    private static int POSICION_Y=68;
-    public BotonIzquierda(String text, Skin skin) {
+    private int POSICION_X;
+    private int POSICION_Y;
+    private static int ANCHO=300;
+    private static int ALTO=300;
+    public BotonIzquierda(String text, Skin skin, MyGdxGame game) {
         super(text, skin);
+        POSICION_X;
         this.setPosition(POSICION_X,POSICION_Y);
+        this.setSize(ANCHO,ALTO);
+        this.addCaptureListener();
     }
 }
