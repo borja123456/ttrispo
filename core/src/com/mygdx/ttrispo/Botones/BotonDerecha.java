047ef88dabfea8712b2ee28502e2ab4bf1bd7c16
JorgeCarreras2 <jorgecarrerasmartin@hotmail.com>
2019-10-27 18:57:53 +0100

    Botones

--- core/src/com/mygdx/ttrispo/Botones/BotonDerecha.java
+++ core/src/com/mygdx/ttrispo/Botones/BotonDerecha.java
@@ -6,22 +6,20 @@ import com.badlogic.gdx.scenes.scene2d.ui.Skin;
 import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
 import com.badlogic.gdx.scenes.scene2d.utils.ChangeListener;
 import com.mygdx.ttrispo.Gestores.GestorEstado;
-import com.mygdx.ttrispo.MyGdxGame;
-import com.mygdx.ttrispo.Pantalla.Partida;
 
-public class BotonIzquierda extends TextButton {
+public class BotonDerecha extends TextButton {
     private static int POSICION_X= Gdx.graphics.getWidth();
     private int POSICION_Y = Gdx.graphics.getHeight();
     private static int ANCHO=300;
     private static int ALTO=300;
-    public BotonIzquierda(String text, Skin skin, final GestorEstado gestorEstado) {
+    public BotonDerecha(String text, Skin skin, final GestorEstado gestorEstado) {
         super(text, skin);
         this.setPosition(POSICION_X,POSICION_Y);
         this.setSize(ANCHO,ALTO);
         this.addCaptureListener(new ChangeListener() {
             @Override
             public void changed(ChangeEvent event, Actor actor) {
-                gestorEstado.setEstado(gestorEstado.IZQUIERDA);
+                gestorEstado.setEstado(gestorEstado.DERECHA);
             }
         });
     }
