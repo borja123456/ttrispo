e05aa908c1f39929ec07cd618b4f187c513f086f
JorgeCarreras2 <jorgecarrerasmartin@hotmail.com>
2019-10-28 09:15:47 +0100

    arriba yo

--- core/src/com/mygdx/ttrispo/Botones/BotonDerecha.java
+++ core/src/com/mygdx/ttrispo/Botones/BotonDerecha.java
@@ -8,10 +8,10 @@ import com.badlogic.gdx.scenes.scene2d.utils.ChangeListener;
 import com.mygdx.ttrispo.Gestores.GestorEstado;
 
 public class BotonDerecha extends TextButton {
-    private static int POSICION_X= Gdx.graphics.getWidth();
-    private int POSICION_Y = Gdx.graphics.getHeight();
-    private static int ANCHO=300;
-    private static int ALTO=300;
+    private final int POSICION_X= Gdx.graphics.getWidth();
+    private final int POSICION_Y = Gdx.graphics.getHeight();
+    private final static int ANCHO=300;
+    private final static int ALTO=300;
     public BotonDerecha(String text, Skin skin, final GestorEstado gestorEstado) {
         super(text, skin);
         this.setPosition(POSICION_X,POSICION_Y);
